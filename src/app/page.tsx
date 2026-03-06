11b10239023fd5fa2308b140dd5b59d7136e3879
pvcvillamanan@gmail.com <paola@Paola.localdomain>
2026-03-06 21:21:07 +0000

    refactor: remove JSON-LD structured data from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,8 @@
 import Hero from "./components/Hero";
-import Header from "./components/Header";
 
 export default function HomePage()  {
   return (
     <>
-    <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "Person",
-            name: "Paola Campos",
-            url: "https://paolacampos.dev",
-            image: "https://paolacampos.dev/favicon.png",
-            jobTitle: "Web Developer",
-            sameAs: [
-              "https://www.paolacampos.com"
-            ]
-          }),
-        }}
-      />
     <main>
       <Hero />
     </main>
